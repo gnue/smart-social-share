f84cbfc663ac55b1a96bf9d4128ffcd8b08f1c58
gnue <gnue@so-kukan.com>
2012-05-03 19:35:05 +0900

    facebook のスクリプトの出力方法を echo から html 埋込みに変更

--- smart_social_share.php
+++ smart_social_share.php
@@ -267,15 +267,16 @@ class SmartSocialShare extends SmartSocialShareBase {
 	/// Facebook SDK の追加
 	function add_fb_script() {
 		$locale = $this->blog_locale();
-
-		echo '<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/'.$locale.'/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, "script", "facebook-jssdk"));</script>';
+		?>
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/<?php echo $locale; ?>/all.js#xfbml=1";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, "script", "facebook-jssdk"));</script>
+		<?php
 	}
 
 	/// スクリプトを追加
